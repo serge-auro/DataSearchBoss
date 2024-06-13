a201ccd98757b479200feb67b5ee8d10e4e615c3
Buddyroo <buddyroo@yandex.ru>
2024-06-13 15:01:08 -0300

    отдельные видео

--- HANDLE_ONE_only_video.py
+++ HANDLE_ONE_only_video.py
@@ -2,14 +2,19 @@ import json
 import time
 import os
 import logging
+import requests
+
+
+
 from download_video_by_url_and_make_frames import create_thumbnails_for_video_message, get_video_duration
 from upload_only_VIDEO_vector import process_only_video_data, delete_frames
 
 # Настройка логирования
 logging.basicConfig(filename='processing.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-vectors_file_paths = ['vectors_5.json', 'vectors_6.json']
-statistics_file_path = 'statistics_video_56.json'
+vectors_file_path = 'vectors_separated_frames_1.json'
+statistics_file_path = 'statistics_separated_frames.json'
+unprocessed_videos_log = 'unprocessed_videos.log'
 
 def load_json(file_path):
     if not os.path.exists(file_path):
@@ -25,21 +30,70 @@ def load_json(file_path):
             logging.error(f"Error loading JSON file {file_path}: {str(e)}")
             return {}
 
-
 def save_json(data, file_path):
     with open(file_path, 'w', encoding='utf-8') as file:
         json.dump(data, file, indent=4)
 
-
 def load_last_state():
-    vectors_5 = load_json(vectors_file_paths[0])
-    vectors_6 = load_json(vectors_file_paths[1])
+    vectors = load_json(vectors_file_path)
     statistics = load_json(statistics_file_path)
-    return vectors_5, vectors_6, statistics
+    return vectors, statistics
 
+def log_unprocessed_video(video_id):
+    with open(unprocessed_videos_log, 'a', encoding='utf-8') as file:
+        file.write(f"{video_id}\n")
+
+def process_only_video_data(video_id):
+    url = "http://127.0.0.1:8000/encode"
+    json_file_path = 'video_description/all_videos.json'
+    frames_dir = "frames"
+
+    with open(json_file_path, 'r', encoding='utf-8') as file:
+        all_videos = json.load(file)
+
+    video_url = all_videos.get(video_id, {}).get('url', None)
+    text = None
+
+    files = []
+    file_handles = []
+    try:
+        for filename in os.listdir(frames_dir):
+            if filename.startswith(f'key_frame_{video_id}_') and filename.endswith(('.jpg', '.jpeg', '.png')):
+                file_path = os.path.join(frames_dir, filename)
+                file_handle = open(file_path, 'rb')
+                files.append(('images', (filename, file_handle, 'image/jpeg')))
+                file_handles.append(file_handle)
+        data = {'texts': [text]}
+
+        response = requests.post(url, files=files, data=data)
+        if response.status_code == 200:
+            print("сейчас получила ответ")
+
+            image_vectors = response.json().get('image_features', None)
+            text_vector = response.json().get('text_features', None)
+            result = True
+        else:
+            log_message = f"Failed to get a proper response. Status code: {response.status_code}\nResponse: {response.text}"
+            print(log_message)
+            logging.error(log_message)
+            image_vectors = None
+            text_vector = None
+            result = False
+    except Exception as e:
+        log_message = f"Error during data processing: {str(e)}"
+        print(log_message)
+        logging.error(log_message)
+        image_vectors = None
+        text_vector = None
+        result = False
+    finally:
+        for file_handle in file_handles:
+            file_handle.close()
+
+    return result, image_vectors, text_vector
 
 def main_handle_videos():
-    vectors_5, vectors_6, statistics = load_last_state()
+    vectors, statistics = load_last_state()
     json_file_path = 'video_description/all_videos.json'
 
     with open(json_file_path, 'r', encoding='utf-8') as file:
@@ -54,29 +108,31 @@ def main_handle_videos():
             logging.error(f"Error loading JSON file {json_file_path}: {str(e)}")
             return
 
-    video_ids = list(all_videos.keys())[4000:6000]
+    last_processed_id = max(vectors.keys(), default=None)
+    start_index = 0
 
-    for i, video_id in enumerate(video_ids):
-        if video_id in vectors_5 or video_id in vectors_6:
-            continue  # Skip already processed videos
+    if last_processed_id:
+        video_ids = list(all_videos.keys())
+        start_index = video_ids.index(last_processed_id) + 1
+    else:
+        video_ids = list(all_videos.keys())[:100]
+
+    for i in range(start_index, len(video_ids)):
+        video_id = video_ids[i]
+        if video_id in vectors:
+            continue  # Пропустить уже обработанные видео
 
         start_time = time.time()
 
         output_folder = "frames"
         frames, video_duration, frames_count = create_thumbnails_for_video_message(video_id, all_videos[video_id]['url'], output_folder)
 
-        success, vector = process_only_video_data(video_id)
-        if success and vector is not None:
-            if i < 2000:
-                vectors_5[video_id] = {
-                    "url": all_videos[video_id]['url'],
-                    "vector": vector
-                }
-            else:
-                vectors_6[video_id] = {
-                    "url": all_videos[video_id]['url'],
-                    "vector": vector
-                }
+        success, image_vectors, text_vector = process_only_video_data(video_id)
+        if success and image_vectors is not None:
+            vectors[video_id] = {
+                "url": all_videos[video_id]['url'],
+                "vectors": image_vectors  # Список тензоров (списков) для каждого изображения
+            }
             delete_frames(output_folder, video_id)
             log_message = f"Successfully processed data for {video_id} and frames deleted."
             print(log_message)
@@ -85,11 +141,10 @@ def main_handle_videos():
             log_message = f"Data for {video_id} was not processed, frames remain in the folder."
             print(log_message)
             logging.warning(log_message)
+            log_unprocessed_video(video_id)
 
         end_time = time.time()
         total_time = end_time - start_time
-
-
 
         statistics[video_id] = {
             "processing_time": total_time,
@@ -106,10 +161,8 @@ def main_handle_videos():
         logging.info(log_message)
 
         # Сохранение данных после обработки каждого видео
-        save_json(vectors_5, vectors_file_paths[0])
-        save_json(vectors_6, vectors_file_paths[1])
+        save_json(vectors, vectors_file_path)
         save_json(statistics, statistics_file_path)
-
 
 if __name__ == "__main__":
     try:
